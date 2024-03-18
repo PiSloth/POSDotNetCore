84fb0593da4c6cd6c777d1495d9bd2a4c0b031e4
ProMax Developer <dev@ProMaxs-Mac-mini.local>
2024-03-18 16:25:41 +0630

    [Complete] Asp.Net MCV CRUD

--- POSDotNetCore.MvcApp/Controllers/BlogController.cs
+++ POSDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -19,6 +19,69 @@ namespace POSDotNetCore.MvcApp.Controllers
            List<BlogDataModelOnMac> lst = _db.Blogs.ToList();
             return View("BlogIndex" , lst);
 		}
+
+		[ActionName("Edit")]
+		public IActionResult BlogEdit(int id)
+		{
+			BlogDataModelOnMac item = _db.Blogs.FirstOrDefault(x => x.BlogId == id)!;
+			if(item is null)
+			{
+				return Redirect("/Blog");
+			}else
+			{
+				return View("BlogEdit", item);
+			}
+		}
+
+		[ActionName("Create")]
+		public IActionResult BlogCreate()
+		{
+			return View("BlogCreate");
+		}
+
+		[HttpPost]
+		[ActionName("Save")]
+		public IActionResult BlogSave(BlogDataModelOnMac blog)
+		{
+			_db.Add(blog);
+			_db.SaveChanges();
+			return Redirect("/Blog");
+		}
+
+		[HttpPost]
+		[ActionName("Update")]
+		public IActionResult BlogUpdate(int id, BlogDataModelOnMac blog)
+		{
+			BlogDataModelOnMac item = _db.Blogs.FirstOrDefault(x => x.BlogId == id)!;
+			if (item is null)
+			{
+				return Redirect("/Blog");
+			}else
+			{
+				item.BlogAuthor = blog.BlogAuthor;
+				item.BlogTitle = blog.BlogTitle;
+				item.BlogContent = blog.BlogContent;
+				_db.SaveChanges();
+
+				return Redirect("/Blog");
+			}
+		}
+
+		[ActionName("Delete")]
+		public IActionResult BlogDelete(int id)
+		{
+			BlogDataModelOnMac item = _db.Blogs.FirstOrDefault(x => x.BlogId == id)!;
+			if (item is null)
+			{
+				return Redirect("/Blog");
+			}else
+			{
+				_db.Remove(item);
+				_db.SaveChanges();
+
+				return Redirect("/Blog");
+			}
+		}
     }
 }
 
